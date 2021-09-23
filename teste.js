4396bc1cbe56cbf201b74b049c0a3e91573dcca8
BrunoHenSL <brunosouz16@hotmail.com>
2021-09-23 00:57:04 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,8 @@
+alert("Página em dedicação ao Batman!")
+
+const nameReal = 'Bruce Wayne';
+const codName = 'Batman';
+let city = 'Gothan';
+let year = 25;
+
+console.log (nameReal, codName, city, year);
